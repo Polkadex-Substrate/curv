41abc757181b193007b047e32f978e14d7397d58
Gautham J <Gauthamastro@gmail.com>
2021-07-17 13:00:41 +0530

    chore: serde is happy for now

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,12 @@
 */
 
 #![allow(clippy::upper_case_acronyms)]
+#![cfg_attr(not(target_env = "sgx"), no_std)]
+#![cfg_attr(target_env = "sgx", feature(rustc_private))]
+
+#[cfg(not(target_env = "sgx"))]
+#[macro_use]
+extern crate sgx_tstd as std;
 
 pub mod elliptic;
 
